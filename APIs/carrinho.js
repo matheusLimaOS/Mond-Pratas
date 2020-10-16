ec54df77d9edbd3bbd1960bb74a5ee16532ddc84
matholaslima <matholaslima@users.noreply.github.com>
2020-10-15 23:46:22 -0300

    Termino da refatoração para uso de APIs/Tela Carrinho

--- APIs/carrinho.js
+++ APIs/carrinho.js
@@ -16,5 +16,39 @@ app.get("/carrinho/:user",(req,res) => {
             }
         })
 })
+app.delete("/carrinho/:user",(req, res) => {
+    let user = req.params.user;
+
+    connection.query("delete from carrinho where usuario = '" + user + "' ;",
+        function (error,data){
+            if(error){
+                res.status(500);
+                res.json({});
+            }
+            else{
+                res.status(200);
+                res.json({});
+            }
+        }
+    )
+})
+
+app.delete("/carrinho/:user/:id",(req, res) => {
+    let user = req.params.user;
+    let idc = req.params.id;
+
+    connection.query("delete from carrinho where usuario = '" + user + "' and id_produto = "+ idc +" ;",
+        function (error,data){
+            if(error){
+                res.status(500);
+                res.json({});
+            }
+            else{
+                res.status(200);
+                res.json({});
+            }
+        }
+    )
+})
 
 module.exports = app;
--- APIs/produto.js
+++ APIs/produto.js
@@ -43,6 +43,7 @@ app.put("/produto/:id",async (req,res) => {
     let qtdf = 0 ;
     let flag = 0 ;
     let flag2 = 0;
+    let data = new Date();
 
    if(valor === '0' && escolha !== -1){
         res.status(403);
@@ -79,7 +80,7 @@ app.put("/produto/:id",async (req,res) => {
             return new Promise((resolve, reject) => {
                 connection.query("select * from carrinho where id_produto= " + idc + " and usuario = '"+ user +"';",
                 function (error,data){
-                    if(data[0]!==undefined && data[0] !== []){
+                    if(data[0]!==undefined){
                         resolve(data);
                     }
                     else{
@@ -109,6 +110,8 @@ app.put("/produto/:id",async (req,res) => {
             else{
                 let qtd2 = parseInt(flag2[0].prod_quant) + parseInt(qtd);
                 connection.query("update carrinho set prod_quant = " + qtd2 + " where id_carrinho = " + flag2[0].id_carrinho);
+                res.status(200);
+                res.json({});
             }
         } else {
             connection.query("update produtos set quantidade = " + qtdf + " where ID = " + idc + ";");
@@ -117,5 +120,84 @@ app.put("/produto/:id",async (req,res) => {
         }
     }
 })
+
+app.put("/produto/carrinho/:id",async (req,res) => {
+    let idc = req.params.id;
+    let qtd = req.body.quant;
+    let valor = req.body.valor;
+    let user = req.body.user;
+    let descri = req.body.descricao;
+    let quantb = 1;
+    let qtdf = 0 ;
+
+
+    if (qtd < 0) {
+        res.status(405);
+        res.json({});
+    }
+
+    try {
+        qtdf = await banco();
+    } catch (error) {
+        console.log("TESTE" + error);
+    }
+
+    function banco() {
+        return new Promise((resolve, reject) => {
+            connection.query("select * from produtos where ID = " + idc + " ;",
+                function (error, data) {
+                    quantb = data[0].quantidade;
+                    qtdf = quantb - parseInt(qtd);
+                    resolve(qtdf);
+                    reject(error);
+                })
+        })
+    }
+
+    if (qtdf < 0) {
+        res.status(409);
+        res.json({});
+    }
+    else {
+        try {
+            await update(qtdf,idc);
+            await histvenda(idc,descri,qtd,valor*qtd,user);
+            res.status(200);
+            res.json({});
+        } catch (error) {
+            console.log(error);
+            res.status(500);
+            res.json({});
+        }
+    }
+})
+
+function update(qtdf,idprod){
+    return new Promise((resolve, reject) => {
+        connection.query("update produtos set quantidade = "+ qtdf +" where ID = "+ idprod +" ;",
+            function (error, data) {
+                if (error) {
+                    reject(error);
+                }
+                else{
+                    resolve(1);
+                }
+            });
+    })
+}
+
+function histvenda(idprod,descriprod,qtdvend,valven,user){
+    return new Promise((resolve, reject) => {
+        connection.query("insert into vendas values(NULL,"+ idprod +", '"+ descriprod +"' , "+ qtdvend +" , "+  valven +", '"+ user +"',DEFAULT);",
+            function (error){
+                if(error){
+                    reject(error);
+                }
+                else{
+                    resolve(1);
+                }
+            })
+    })
+}
 
 module.exports = app;
