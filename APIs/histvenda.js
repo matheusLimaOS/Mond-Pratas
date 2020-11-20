b1cf17b6b2418d4e3eb37e6a269b06c2108484ad
Matheus Lima Oliveira e Souza <matheussouza@sistemafieg.net>
2020-11-20 17:08:58 -0300

    Adicionando gitignore

--- APIs/histvenda.js
+++ APIs/histvenda.js
@@ -22,31 +22,40 @@ app.get("/histovenda",(req, res) => {
         })
 })
 
-app.post("/venda/:user",(req, res) => {
+app.post("/venda/:user",async (req, res) => {
     let user = req.params.user;
+    let num=0;
+    console.log("1");
+    connection.query("select count(*) as conta from vendas_itens where id_venda=-1",
+        await function (error,data){
+            console.log("2");
+            num=data[0].conta;
+    })
 
-    connection.query("insert into vendas values(NULL, -1 , -1, '"+ user +"',DEFAULT);",
-        function (error,data){
-            if(error){
-                res.json({});
-                res.status(500)
-            }
-            else{
-                try {
-                    connection.query("update vendas_itens set ID_venda = " + data.insertId + " where ID_venda = -1");
-                    connection.query("update vendas set qtd_vendido = (select sum(qtd_vendido) from vendas_itens where ID_venda = " + data.insertId + ")," +
-                        "val_vendido = (select sum(valor_vendido) from vendas_itens where ID_venda = " + data.insertId + ") " +
-                        "where ID_venda = " + data.insertId);
-                    res.status(200);
+    console.log("3");
+
+    if(num!==0) {
+        connection.query("insert into vendas values(NULL, -1 , -1, '" + user + "',DEFAULT);",
+            function (error, data) {
+                if (error) {
                     res.json({});
-                }
-                catch (err){
-                    console.log(err);
-                }
+                    res.status(500)
+                } else {
+                    try {
+                        connection.query("update vendas_itens set ID_venda = " + data.insertId + " where ID_venda = -1");
+                        connection.query("update vendas set qtd_vendido = (select sum(qtd_vendido) from vendas_itens where ID_venda = " + data.insertId + ")," +
+                            "val_vendido = (select sum(valor_vendido) from vendas_itens where ID_venda = " + data.insertId + ") " +
+                            "where ID_venda = " + data.insertId);
+                        res.status(200);
+                        res.json({});
+                    } catch (err) {
+                        console.log(err);
+                    }
 
+                }
             }
-        }
-    )
+        )
+    }
 })
 
 
